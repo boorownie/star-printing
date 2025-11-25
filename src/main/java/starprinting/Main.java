1fb28577b68cefaea13e211f419c59ac454e77e9
boorownie <probitanima11@gmail.com>
2025-11-25 20:04:03 +0900

    default code

---
+++ src/main/java/starprinting/Main.java
@@ -0,0 +1,12 @@
+package starprinting;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: 요구사항에 맞게 구현하세요.
+        // *
+        // **
+        // ***
+        // ****
+        // *****
+    }
+}
